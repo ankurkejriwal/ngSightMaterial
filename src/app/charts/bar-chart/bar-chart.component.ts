c968986fad969782f2e54f600b2f8963d7e1d72d
Ankur Kejriwal <ankurkejriwal@ankurs-mbp.lan>
2021-07-20 16:11:37 -0400

    Working linechart and pie comps

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -11,10 +11,61 @@ export class BarChartComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts;
   chartOptions: Highcharts.Options = {
     series: [{
-      data: [1, 2, 3],
+      name : 'Year 1800',
+      data: [107, 31, 635, 203, 2],
       type: 'bar'
-    }]
-  };
+    },{
+      name: 'Year 1900',
+      data: [133, 156, 947, 408, 6],
+      type: 'bar'
+    },{
+      name: 'Year 2016',
+      data: [1216, 1001, 4436, 738, 40],
+      type: 'bar'
+    },{
+      name: 'Year 2000',
+      data: [814, 841, 3714, 727, 31],
+      type: 'bar'
+    },],
+    title: {
+      text: 'Historic World Population by Region'
+    },
+    subtitle: {
+      text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+  },
+  xAxis: {
+    categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+    title: {
+        text: null
+    }},
+yAxis: {
+    min: 0,
+    title: {
+        text: 'Population (millions)',
+        align: 'high'
+    },
+    labels: {
+        overflow: 'justify'
+    }
+},
+tooltip: {
+    valueSuffix: ' millions'
+},
+plotOptions: {
+    bar: {
+        dataLabels: {
+            enabled: true
+        }
+    }
+},
+credits: {
+    enabled: false
+},
+exporting:{
+  enabled : true,
+}
+
+};
 
   constructor() { }
 
